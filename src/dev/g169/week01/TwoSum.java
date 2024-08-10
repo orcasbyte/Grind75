0e46294f76400158673035dc7ae716ca9b5d9cb6
Shashank Shekhar <shashank.jfsd@gmail.com>
2024-08-11 03:37:24 +0530

    two-sum-✅

---
+++ src/dev/g169/week01/TwoSum.java
@@ -0,0 +1,37 @@
+package dev.g169.week01;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <h1>Two Sum</h1>
+ * <p><span>🔗 Link: </span><span>https://leetcode.com/problems/two-sum/description/</span></p>
+ * <p><span>💪🏼 Difficulty: </span><strong style="color: green">Easy</strong></p>
+ * <p><span>🏷️ Topics: </span><span>Array, Hash Table</span></p>
+ */
+public class TwoSum {
+
+    private static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int numToFind = target - nums[i];
+
+            if (hashMap.containsKey(numToFind))
+                return new int[] {hashMap.get(numToFind), i};
+            else
+                hashMap.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+}
